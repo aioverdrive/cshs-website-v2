17cb01a9a3f947b7fe8439f996d152478b41428e
aioverdrive <141070467+aioverdrive@users.noreply.github.com>
2024-08-28 14:02:19 -0700

    better cards and images
    added condensed images and made cards a tad fancy and interactive

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,6 @@
-//  const logoArray = 
-// [   "/images/CSHS_Logo_square_letters.webp", 
-//     "/images/GWCLogo.webp",
-//     "/images/image-removebg-preview.png",
-//     "/images/nevadaTSALogo.webp"];
-//     let logoArrayIndex = 0;
-//     const imageEl = document.querySelector('#image-el');
-//     const imageContainer = document.querySelector('#img-container');
-//     imageContainer.addEventListener('click', function(){
-//         if (logoArrayIndex === 3){
-//             imageEl.src = logoArray[0];
-//             logoArrayIndex = 0;
-//         }
-//         else {
-//             logoArrayIndex++;
-//             imageEl.src = logoArray[logoArrayIndex];
-//         }
-//     })
+const aboutUsSection = document.getElementById('about-clubs');
+const clubCards = document.getElementsByClassName('club-card');
+// slider logic
 const left = document.getElementById("left-side");
 
 const handleMove = e => {
@@ -25,3 +10,14 @@ const handleMove = e => {
 document.onmousemove = e => handleMove(e);
 
 document.ontouchmove = e => handleMove(e.touches[0]);
+
+aboutUsSection.addEventListener('mouseover', function(event){
+  if (event.target.classList.contains('club-card') || event.target.classList.contains('club-content')){
+    aboutUsSection.style.backgroundColor = 'red';
+  }
+})
+aboutUsSection.addEventListener('mouseout', function(event){
+  if (event.target.classList.contains('club-card')){
+    aboutUsSection.style.backgroundColor = '#CAF0F8'
+  }
+})
