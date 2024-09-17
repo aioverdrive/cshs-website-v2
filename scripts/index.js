2748e6414319436c767e8fb8becdd17da9464ffb
aioverdrive <141070467+aioverdrive@users.noreply.github.com>
2024-09-17 10:07:25 -0700

    lot of stuff
    added foundations for caorusel and adding linear gradients in diagrams
    also i divdided the css files lol

--- scripts/index.js
+++ scripts/index.js
@@ -13,11 +13,25 @@ document.ontouchmove = e => handleMove(e.touches[0]);
 
 aboutUsSection.addEventListener('mouseover', function(event){
   if (event.target.classList.contains('club-card') || event.target.classList.contains('club-content')){
-    aboutUsSection.style.backgroundColor = 'red';
+    console.log("fired")
+    switch (event.target.id) {
+      case "cshs":
+        aboutUsSection.style.background = "linear-gradient(45deg in lch, #62C6FE, #1666A8)";
+        break;
+      case "gwc":
+        aboutUsSection.style.background = "linear-gradient(-45deg in lch, #0ca096, #FF69B4)";
+        break;
+      case "skillsusa":
+        // #DF0B13
+        aboutUsSection.style.background = "linear-gradient(-45deg in lch, #DF0B13, #104481)";
+        break;
+      case "tsa":
+        // #F12F23
+        aboutUsSection.style.background = "linear-gradient(45deg in lch, #F12F23, #005FAC, #FEFEFF)";
+        break;
+    }
   }
-})
-aboutUsSection.addEventListener('mouseout', function(event){
-  if (event.target.classList.contains('club-card')){
-    aboutUsSection.style.backgroundColor = '#CAF0F8'
+  else {
+    aboutUsSection.style.background = '#CAF0F8';
   }
 })
